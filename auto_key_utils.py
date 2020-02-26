74b61ffc4b7ff2c97a8e94a598891de5a4d850a8
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-26 17:20:32 -0600

    added sm

---
+++ auto_key_utils.py
@@ -0,0 +1,159 @@
+''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Header -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
+import sys, os    ;     sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__)))) # to allow for relative imports, delete any imports under this line
+
+util_submodule_l = ['clipboard_utils'] # list of all imports from local util_submodules that could be imported elsewhere to temporarily remove from sys.modules
+
+# temporarily remove any modules that could conflict with this file's local util_submodule imports
+og_sys_modules = sys.modules    ;    pop_l = [] # save the original sys.modules to be restored at the end of this file
+for module_descrip in sys.modules.keys():  
+    if any( util_submodule in module_descrip for util_submodule in util_submodule_l )    :    pop_l.append(module_descrip) # add any module that could conflict local util_submodule imports to list to be removed from sys.modules temporarily
+for module_descrip in pop_l    :    sys.modules.pop(module_descrip) # remove all modules put in pop list from sys.modules
+util_submodule_import_check_count = 0 # count to make sure you don't add a local util_submodule import without adding it to util_submodule_l
+
+''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard: Local Utility Submodule Imports  -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
+
+from util_submodules.clipboard_utils import clipboard_utils as cu    ; util_submodule_import_check_count += 1 
+    
+''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+if util_submodule_import_check_count != len(util_submodule_l)    :    raise Exception("ERROR:  You probably added a local util_submodule import without adding it to the util_submodule_l")
+''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+    
+    
+
+
+
+import keyboard as k
+from pynput.keyboard import Key
+from pynput.keyboard import Controller
+import time
+import _tkinter
+
+
+
+
+def make_selection(select_mode, num_arrows = None):
+    def shift_home_select():
+        keyboard = Controller()
+        with keyboard.pressed(Key.shift):
+            keyboard.press(Key.home)
+            keyboard.release(Key.shift)
+    
+    if select_mode == 'all':
+        k.press_and_release('Ctrl+a')
+        
+    elif select_mode == 'shift_home':            
+        shift_home_select()
+        
+    elif select_mode == 'end_shift_home':
+        k.press_and_release('end')
+        shift_home_select()
+        
+    elif select_mode == 'end_shift_left_arrow':
+        k.press_and_release('end')
+        keyboard = Controller()
+        keyboard.press(Key.shift)
+        
+        for x in range(num_arrows):
+            keyboard.press(Key.left)
+            keyboard.release(Key.left)
+
+        keyboard.release(Key.shift)
+        
+
+# Ctrl + a, copy to clipboard, return clipboard
+def get_selection(deselect_key_str = None, error_on_empty_clipboard = False):
+    
+    try:
+        og_clipboard = cu.get_clipboard()
+    except(_tkinter.TclError):
+        og_clipboard = ''
+        
+        
+    cu.clear_clipboard()
+    
+    k.press_and_release('Ctrl+c')    
+    
+    time.sleep(.3)
+    
+    new_clipboard = cu.get_clipboard()
+    
+    if error_on_empty_clipboard and str(new_clipboard) == '':
+        raise Exception("ERROR:  Tried to copy current selection, nothing was copied to clipboard")
+        
+    
+    cu.set_clipboard(og_clipboard)
+    
+    if deselect_key_str != None:
+        time.sleep(.3)# need ??????????????????????????????????????????????????????
+        k.press_and_release(deselect_key_str)
+    
+    return new_clipboard
+
+
+
+def make_then_get_selection(select_mode, deselect_key_str = None, error_on_empty_clipboard = False, num_arrows = None):
+    make_selection(select_mode, num_arrows)
+    time.sleep(.3)
+    return get_selection(deselect_key_str, error_on_empty_clipboard)
+
+
+
+
+
+
+
+
+''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Footer -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
+sys.modules = og_sys_modules
+''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
+if __name__ == '__main__':
+    print('In Main:  auto_key_utils')
+    
+# #     print(get_select_all())
+    time.sleep(3)
+#     k.press_and_release('shift+home')
+# #     k.press_and_release('home')
+
+
+#     make_selection('end_shift_left_arrow', num_arrows=3)
+    
+    
+    print(make_then_get_selection('end_shift_left_arrow', num_arrows = 3))
+#     make_selection('end_shift_home', num_arrows=3)
+
+#     k.press('shift')
+#     time.sleep(.3)
+#     k.press_and_release('left arrow')
+#     time.sleep(.3)
+# 
+#     k.press_and_release('left arrow')
+#     time.sleep(.3)
+#     k.press_and_release('left arrow')
+#     time.sleep(.3)
+#     k.release('shift')
+    
+    
+#     k.release('shift')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    print('End of Main:  auto_key_utils')
