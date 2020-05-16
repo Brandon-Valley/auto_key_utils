bd5da27e6101a5f3491eb952474c39ad50065051
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-16 16:57:15 -0500

    switched to usms

--- auto_key_utils.py
+++ auto_key_utils.py
@@ -1,30 +1,14 @@
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Header -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-import sys, os    ;     sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__)))) # to allow for relative imports, delete any imports under this line
-
-util_submodule_l = ['clipboard_utils'] # list of all imports from local util_submodules that could be imported elsewhere to temporarily remove from sys.modules
-
-# temporarily remove any modules that could conflict with this file's local util_submodule imports
-og_sys_modules = sys.modules    ;    pop_l = [] # save the original sys.modules to be restored at the end of this file
-for module_descrip in sys.modules.keys():  
-    if any( util_submodule in module_descrip for util_submodule in util_submodule_l )    :    pop_l.append(module_descrip) # add any module that could conflict local util_submodule imports to list to be removed from sys.modules temporarily
-for module_descrip in pop_l    :    sys.modules.pop(module_descrip) # remove all modules put in pop list from sys.modules
-util_submodule_import_check_count = 0 # count to make sure you don't add a local util_submodule import without adding it to util_submodule_l
-
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard: Local Utility Submodule Imports  -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-
-from util_submodules.clipboard_utils import clipboard_utils as cu    ; util_submodule_import_check_count += 1 
-    
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
-if util_submodule_import_check_count != len(util_submodule_l)    :    raise Exception("ERROR:  You probably added a local util_submodule import without adding it to the util_submodule_l")
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
-    
-
-
 import keyboard as k
 from pynput.keyboard import Key
 from pynput.keyboard import Controller
 import time
 import _tkinter
+
+
+if __name__ == "__main__": 
+    from   usms.clipboard_utils import clipboard_utils as cu
+else:
+    from . usms.clipboard_utils import clipboard_utils as cu
 
 
 
@@ -98,9 +82,6 @@ def make_then_get_selection(select_mode, deselect_key_str = None, error_on_empty
 
 
 
-''' -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- All Utilities Standard Footer -- VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV -- '''
-sys.modules = og_sys_modules
-''' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ '''
 if __name__ == '__main__':
     print('In Main:  auto_key_utils')
     
